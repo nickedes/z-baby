c4fb71dca6d990c1ee07962eb1c79bbc6f6ef47a
nikhil <nkmittal4994@gmail.com>
2015-05-26 11:00:15 +0530

    site map

--- start.py
+++ start.py
@@ -7,9 +7,24 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/hello')
-def hello():
-    return 'Hello World!'
+@app.route('/about/')
+@app.route('/about/<name>')
+def about(name):
+    if name == "ZIIEI":
+        return render_template('ziiei.html')
+    elif name == "Aurbindo":
+        return render_template('aurbindo.html')
+    elif name == "Litchi":
+        return render_template('litchi.html')
+    elif name == "UP Govt":
+        return
+
+
+@app.route('/ZIIEI/')
+@app.route('/ZIIEI/<page>'):
+def ziiei(page):
+    if page = "workflow":
+        return
 
 if __name__ == '__main__':
     app.run(debug=True)
