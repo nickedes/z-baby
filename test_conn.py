a779bf9beeda60fd798ec55869455f84fc08f5de
kwikadi <kwikadi@gmail.com>
2015-05-27 16:26:01 +0530

    DB Connection

--- test_conn.py
+++ test_conn.py
@@ -1,10 +1,26 @@
-from os import getenv
+import os
 import pymssql
+from configparser import ConfigParser
 
-server = getenv("103.247.98.43:5544")
-user = getenv("mssql_licthi")
-password = getenv("Mssql@mayank@123")
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+config = ConfigParser()
+config.read(os.path.join(CURRENT_DIR, 'config.ini'))
+
+server = config["Database"]["server"]
+user = config["Database"]["user"]
+password = config["Database"]["password"]
 
 conn = pymssql.connect(server, user, password, "ziiei")
 cursor = conn.cursor()
-print conn
+'''
+cursor.execute(
+    "INSERT INTO dbo.Language VALUES (%s, %d, %s, DEFAULT)",
+    ('English', 1, 'lolpol'))
+
+conn.commit()
+'''
+cursor.execute('SELECT * FROM dbo.Language')
+row = cursor.fetchone()
+print(row)
+conn.close()
