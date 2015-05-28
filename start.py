3e98cc1d6a1b89eeb1ff1c36bec931bd6525f68f
nikhil <nkmittal4994@gmail.com>
2015-05-28 16:45:04 +0530

    Regsiter A basic registration page.

--- start.py
+++ start.py
@@ -10,21 +10,32 @@ from flask import (
 
 from errors import showerrors
 import values
+vals1 = {}
+vals2 = []
+vals3 = []
+vals4 = []
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 showerrors(app)
 
+
 @app.route('/')
 def index():
     session['Language'] = 1
+    global vals1
+    global vals2
+    global vals3
+    global vals4
     vals1, vals2, vals3, vals4 = values.retrieveValues(session['Language'])
-    return render_template('index.html',labels=vals1, menu=vals2, submenu=vals3, menulist=vals4)
+    return render_template('index.html', labels=vals1, menu=vals2, submenu=vals3, menulist=vals4)
+
 
 @app.route('/about')
 def about():
     return render_template('index.html')
+
 
 @app.route('/ziiei/<page>')
 def ziiei(page="workflow"):
@@ -42,7 +53,10 @@ def ziiei(page="workflow"):
 
 @app.route('/register')
 def register():
-    return
+    session['Language'] = 1
+    register_labels = values.getRegisteration_Labels(session['Language'])
+    return render_template('register.html',register_labels=register_labels,labels=vals1, menu=vals2, submenu=vals3, menulist=vals4)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -53,10 +67,12 @@ def login():
         # check if username exists
         session["username"] = username
 
+
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
+
 
 @app.route('/faq')
 def faq():
--- values.py
+++ values.py
@@ -3,28 +3,45 @@ import datetime
 import pymssql
 from configparser import ConfigParser
 
+
+def connection():
+    CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+    config = ConfigParser()
+    config.read(os.path.join(CURRENT_DIR, 'config.ini'))
+
+    server = config["Database"]["server"]
+    user = config["Database"]["user"]
+    password = config["Database"]["password"]
+    conn = pymssql.connect(server, user, password, "ziiei")
+    return conn
+
+
 def retrieveValues(lang=1):
-	CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+    conn = connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        'SELECT LabelType, LabelValue FROM dbo.Label where LanguageID=%d' % lang)
+    labels = cursor.fetchall()
+    cursor.execute(
+        'SELECT MenuID, FormName, FormLink FROM dbo.Menu where LanguageID=%d ORDER BY MenuID' % lang)
+    menu = cursor.fetchall()
+    cursor.execute(
+        'SELECT MenuID, FormName, FormLink FROM dbo.SubMenu where LanguageID=%d ORDER BY SubMenuID' % lang)
+    submenu = cursor.fetchall()
+    conn.close()
+    menulist = []
+    for menuitem in menu:
+        menulist.append(0)
+    for submenuitem in submenu:
+        menulist[submenuitem[0]-1] = 1
 
-	config = ConfigParser()
-	config.read(os.path.join(CURRENT_DIR, 'config.ini'))
+    return {label[0]: label[1] for label in labels}, menu, submenu, menulist
 
-	server = config["Database"]["server"]
-	user = config["Database"]["user"]
-	password = config["Database"]["password"]
-	conn = pymssql.connect(server, user, password, "ziiei")
-	cursor = conn.cursor()
-	cursor.execute('SELECT LabelType, LabelValue FROM dbo.Label where LanguageID=%d' %lang)
-	labels = cursor.fetchall()
-	cursor.execute('SELECT MenuID, FormName, FormLink FROM dbo.Menu where LanguageID=%d ORDER BY MenuID' %lang)
-	menu = cursor.fetchall()
-	cursor.execute('SELECT MenuID, FormName, FormLink FROM dbo.SubMenu where LanguageID=%d ORDER BY SubMenuID' %lang)
-	submenu = cursor.fetchall()
-	conn.close()
-	menulist = []
-	for menuitem in menu:
-		menulist.append(0)
-	for submenuitem in submenu:
-		menulist[submenuitem[0]-1] = 1
 
-	return { label[0]:label[1] for label in labels }, menu, submenu, menulist
+def getRegisteration_Labels(lang=1):
+    conn = connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM dbo.Label where LanguageID = %d and LabelType = 'Registration_field' or LabelType = 'Gender'" % lang)
+    labels = cursor.fetchall()
+    return labels
