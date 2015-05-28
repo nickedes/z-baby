4e737e4ddec93113a3ec67d7b71fd7afb35063be
nikhil <nkmittal4994@gmail.com>
2015-05-28 15:37:11 +0530

    Labels Pushed More labels for registration in DB.

--- test_conn.py
+++ test_conn.py
@@ -30,10 +30,36 @@ cursor.executemany(
     (1, 5, 4, 'Examples', '/examples', 1, 'admin', str(datetime.datetime.now())), 
     (1, 5, 5, 'Terms and Conditions', '/terms', 1, 'admin', str(datetime.datetime.now()))
     ])
+,
+
+conn.commit()
+
+cursor.executemany(
+    "INSERT INTO dbo.Label VALUES (%d, %d, %s, %s, %s, %s)",
+    [
+    (4, 1, 'Registration_field', 'SchoolName', 'admin', str(datetime.datetime.now())),
+    (5, 1, 'Registration_field', 'SchoolAddress', 'admin', str(datetime.datetime.now())),
+    (6, 1, 'Registration_field', 'PhoneNumber', 'admin', str(datetime.datetime.now())),
+    (7, 1, 'Registration_field', 'DateOfJoining', 'admin', str(datetime.datetime.now())),
+    (8, 1, 'Registration_field', 'Awards', 'admin', str(datetime.datetime.now())),
+    (9, 1, 'Registration_field', 'EmployeeID', 'admin', str(datetime.datetime.now())),
+    (10, 1, 'Registration_field', 'Qualification', 'admin', str(datetime.datetime.now())),
+    (11, 1, 'Registration_field', 'Gender', 'admin', str(datetime.datetime.now())),
+    (12, 1, 'Registration_field', 'ResidentialAddress', 'admin', str(datetime.datetime.now())),
+    (13, 1, 'Registration_field', 'EmailID', 'admin', str(datetime.datetime.now())),
+    (14, 1, 'Registration_field', 'Designation', 'admin', str(datetime.datetime.now())),
+    (15, 1, 'Registration_field', 'BlockID', 'admin', str(datetime.datetime.now())),
+    (16, 1, 'Registration_field', 'DistrictID', 'admin', str(datetime.datetime.now())),
+    (17, 1, 'Registration_field', 'StateID', 'admin', str(datetime.datetime.now())),
+    (18, 1, 'Registration_field', 'CountryID', 'admin', str(datetime.datetime.now())),
+    (19, 1, 'Gender', 'Male', 'admin', str(datetime.datetime.now())),
+    (20, 1, 'Gender', 'Female', 'admin', str(datetime.datetime.now())),
+    (21, 1, 'Registration_field', 'Name', 'admin', str(datetime.datetime.now()))
+    ])
 
 conn.commit()
 '''
-cursor.execute('SELECT * FROM dbo.SubMenu ORDER BY MenuID')
+cursor.execute('SELECT * FROM dbo.Label ORDER BY LabelID')
 row = cursor.fetchall()
 print(row)
 conn.close()
