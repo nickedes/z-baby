b31d9df77f04ba6145755192a3d4c3cd1519a83f
nikhil <nkmittal4994@gmail.com>
2015-06-10 17:25:38 +0530

    merge

--- values.py
+++ values.py
@@ -395,7 +395,6 @@ def updateState(CountryID, StateID, StateName):
         return False
     conn.commit()
     return True
-<<<<<<< HEAD
 
 
 def updateDistrict(CountryID, StateID, DistrictID, DistrictName):
@@ -420,5 +419,3 @@ def updateBlock(CountryID, StateID, DistrictID, BlockID, DistrictName):
         return False
     conn.commit()
     return True
-=======
->>>>>>> 6d3d773baeff7d820dbf25fade9662fbb64d7a15
